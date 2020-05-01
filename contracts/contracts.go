909528062179d1dbc07cebab266986b188f6ba6d
Mislav Ilich <mislav.ilich@gmail.com>
2020-05-01 23:23:57 +0200

    Update contracts model

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -1,14 +1,14 @@
 package contracts
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/mislavio/contracter/accounts"
+	"github.com/mislavio/contracter/helpers"
 )
 
 // Contract represents a smart contract published to the contracter API.
 type Contract struct {
-	gorm.Model
+	helpers.BaseModel
 	ABI      postgres.Jsonb `json:"abi"`
 	Bytecode []byte         `json:"bytecode"`
 	Address  string         `json:"address"`
@@ -17,7 +17,7 @@ type Contract struct {
 // MyContract represents the mapping between an Account
 // and a linked Contracter platform Contract
 type MyContract struct {
-	gorm.Model
+	helpers.BaseModel
 	AccountID  string
 	Account    accounts.Account
 	ContractID string
